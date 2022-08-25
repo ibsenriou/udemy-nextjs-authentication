1ede95710577a70969fedfd6681bd6fbbd5b9bc6
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-08-25 17:34:53 -0300

    Finished Project.

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,46 @@
+import { hashPassword } from '../../../lib/auth';
+import connectToDatabase from '../../../lib/db';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message: 'Invalid Input - Password Should Be At Least 7 Chars Long',
+      });
+      return;
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db();
+
+    const existingUser = await db.collection('users').findOne({ email: email });
+
+    if (existingUser) {
+      res.status(422).json({ message: 'User already exists.' });
+      client.close();
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection('users').insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: 'Created User!' });
+    client.close();
+  }
+}
+
+export default handler;
